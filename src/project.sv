455d6f8a424052dbc6e339336ad400830a95f5ce
Anders <anders-code@users.noreply.github.com>
2024-09-21 10:00:06 -0700

    sv

--- src/project.sv
+++ src/project.sv
@@ -17,11 +17,21 @@ module tt_um_anders_tt_6502 (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  reg [7:0]mem[16];
+  reg [7:0]memout;
+  always_ff @(posedge clk) begin
+      if (ui_in[7])
+          mem[ui_in[3:0]] <= uio_in[7:0];
+      memout <= mem[ui_in[3:0]];
+  end
+
+  assign uo_out = memout;
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ui_in[6:4], ena, clk, rst_n, 1'b0};
 
 endmodule
